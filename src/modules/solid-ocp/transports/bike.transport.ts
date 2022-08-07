6843c0d2ac5e3cafde66f4bbdc3cfa7b232268f4
DEMETRA-WORK <1@demetra.work>
2022-08-07 22:20:17 +0600

    update

--- src/modules/solid-ocp/transports/bike.transport.ts
+++ src/modules/solid-ocp/transports/bike.transport.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { ITransportInterface } from '../interfaces/transport.interface';
 import CheckTehDto from '../dto/check-teh.dto';
 @Injectable()
-// Объявляем класс с названием CarTransportService
+// Объявляем класс с названием BikeTransportService
 export class BikeTransportService implements ITransportInterface {
   //Наследуемся от интерфейса
 
   checkTechnicalInspection(params: CheckTehDto) {
-    // В данном методе, проверяем пройдёт ли транспорт тех осмотра, проверяем соответствует ли количество фар, колес и дверей.
+    // В данном методе, проверяем пройдёт ли транспорт тех.осмотр, проверяем соответствует ли количество фар, колес и дверей.
     // Пример: для машины 4 колеса норма, для мотоцикла 2 колеса и т.д
     // То есть метод "checkTechnicalInspection" есть для каждого типа транспорта, но реализует собственную логику проверки тех.осмотра
     const { lights, wheels, doors } = params;
